79c1157101e2c324847863986f30e91089db078f
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-04-08 17:59:37 +0200

    Update whoami.asm

--- whoami.asm
+++ whoami.asm
@@ -1,10 +1,10 @@
 org 40960     ; Applications have org 40960 but
               ; shells have org 32768
 whoami:
-	call 0009h
-	call 0042h
+	call 0009h	; Kernel API: NEWLINE
+	call 0042h	; Kernel API: GET_USERNAME
 	mov ax, si
-	call 0045h
+	call 0045h	; Kernel API: STRING_UPPERCASE
 	mov si, ax
-	call 0003h
+	call 0003h	; Kernel API: PRINT_SI
 ret
